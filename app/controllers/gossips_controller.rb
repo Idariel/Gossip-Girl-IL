e974af428ea698483778e8cd432d9f3311bff708
admin <isalechat@yahoo.com>
2018-01-31 17:53:15 +0100

    qsqsdf

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,21 +1,39 @@
 class GossipsController < ApplicationController
   def home
   end
+
   def new
     @gossip = Gossip.new
   end
+
   def create
-    # @gossip = Gossip.create(anonymous_author: anonymous_author, content: content)
+    # saving data in the controller
     @gossip = Gossip.create(gossip_params)
+    @gossip.save
+    redirect_to @gossip
+
+    # allow and require the title and text parameters for valid use of create
+    private
+    def gossip_params
+      params.require(:gossip).permit(:anonymous_author, :content)
+    end
+
   end
-  def read
+
+
+  def show
     # return a collection with all users
-    @gossip = Gossip.all
+    @gossip = Gossip.find(params[:content])
   end
+
+
   def update
     @gossip = Gossip.find_by(anonymous_author: anonymous_author)
     @gossip.content = 'Dave'
-    @gossip.save  end
+    @gossip.save
+  end
+
+  
   def delete
     @gossip = Gossip.find_by(anonymous_author: anonymous_author)
     @gossip.destroy
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root :to => "gossips#home", as: 'home'
   get '/new', to: 'gossips#new', as: 'new'
   get '/create', to: 'gossips#create', as: 'create'
-  get '/read', to: 'gossips#read', as: 'read'
+  get '/show', to: 'gossips#show', as: 'show'
   get '/update', to: 'gossips#update', as: 'update'
   get '/delete', to: 'gossips#delete', as: 'delete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
