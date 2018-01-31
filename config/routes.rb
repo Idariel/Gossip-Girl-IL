37fec901970bff251da8d914f4ddeec3ac01c49d
admin <isalechat@yahoo.com>
2018-01-31 13:29:24 +0100

    je veux voir mon app dans heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root :to => "home_pages#home", as: 'home'
+  # get '/contact', to: 'home_pages#contact', as: 'contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
